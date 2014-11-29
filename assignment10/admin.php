261957e7efd6f79a0ce866101dac338f944c83e0
Madison Harris <madisonharrisvt@gmail.com>
2014-11-28 21:09:01 -0500

    BLOOPS FROM DATABASE
    Making bloops from the database!

--- assignment10/admin.php
+++ assignment10/admin.php
@@ -2,6 +2,7 @@
 session_start();
 include'top.php';
 include "loggedIn.php";
+include('db.php');
 
 print "<article id = 'main'>";
 
@@ -12,11 +13,32 @@ if (!isset($_SESSION['user'])){
 else{
 
     print "<h1>Account:" . $_SESSION['user']. "</h1>";
-?>
 
-<svg height="50" width="50">
-  <circle cx="25" cy="25" r="15" fill="white" />
-</svg>
+    $query ="SELECT fldDateJoined FROM tblRegister WHERE fldEmailAddress = ?;";
+    $data = array($_SESSION['user']);
+    $results = $thisDatabase->select($query, $data);
+    print "<p>Date Joined: ". $results[0][0] ."</p>";
+    print "<h1>Your Bloop: </h1>";
+
+    $query ="SELECT fldSize, fldCenter, fldRadius, fldColor, fldName FROM tblBloop WHERE pmkRegisterId IN(SELECT DISTINCT pmkRegisterId FROM tblRegister WHERE fldEmailAddress = ?)";
+    $data = array($_SESSION['user']);
+    $results = $thisDatabase->select($query, $data);
+
+    print "<svg height =";
+    print $results[0][0];
+    print " width = ";
+    print $results[0][0];
+    print "> <circle cx = ";
+    print $results[0][1];
+    print " cy = ";
+    print $results[0][1];
+    print " r = ";
+    print $results[0][2];
+    print " fill = ";
+    print $results[0][3];
+    print "  /> </svg>";
+    print "<p>Bloop's Name: " . $results[0][4] . "</p>";
+?>
 </article>
 
 <?php    
